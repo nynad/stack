d03ed8685d66bc5225131beb9a6b4b15be89db8c
nynad <nynadammur@gmail.com>
2024-07-27 13:57:49 -0400

    Add files via upload

---
+++ stack.py
@@ -0,0 +1,7 @@
+# queue structure and stack structures
+# first in first out is queue.
+# last in last out is stack.
+# adding a new item to a stack is known as "pushing"
+# removing is known as "popping"
+# top specicies the Topmost item
+# functionalities include checking the size of the stack, how many items are in it
